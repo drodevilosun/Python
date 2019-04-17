3f8a06be8e09131e39a5477fcbfc01273f5da493
drodevilosun <drodevilosun@gmail.com>
2019-04-18 00:24:57 +0700

    Create Exercise3.py

---
+++ Exercise/Exercise3.py
@@ -0,0 +1,10 @@
+#Exercise 3
+
+print ("Experiment Numer and Math in Python")
+
+print ("Hens", 25 + 30/6)
+print ("Rossters", 100 - 25*3%4)
+
+print ("Compare numerical in print command:")
+print ("Is it greater?", 5 > -2)
+print ("Is it kess or euqal?", 5 <= 2)
